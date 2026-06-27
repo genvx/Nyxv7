a822f25c23d15d1e15732859167a04d5f22be79f
genvx <trulystarize@gmail.com>
2026-06-27 08:48:25 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,239 +1,117 @@
-local j1l1jil1i=Path2DControlPoint.new(UDim2.new(0,0,0,0))
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
+-- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
-    if table.find({'Wave', 'Seliware', 'Volt'}, ({identifyexecutor()})[1]) then
-        getgenv().setthreadidentity = nil
-    end
+	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+		getgenv().setthreadidentity = nil
+	end
 end
 
-local args = ...
-if type(args) == "table" and args.Username then
-    shared.ValidatedUsername = args.Username
-end
-
-if type(args) == "table" and args.Closet then
-    getgenv().Closet = true
-else
-    if getgenv().Closet == nil then
-        getgenv().Closet = false
-    end
-end
-
-local _realLoadstring = clonefunction(loadstring)
 local vape
 local loadstring = function(...)
-    local res, err = _realLoadstring(...)
-    if err and vape then
-        vape:CreateNotification('AeroV4', 'Failed to load : '..err, 30, 'alert')
-    end
-    return res
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
 end
-
--- Integrity check
-do
-    local _hookDetected = false
-    pcall(function()
-        local _testSrc = 'return 1'
-        local _c1 = _realLoadstring(_testSrc)
-        local _c2 = loadstring(_testSrc)
-        if _c1 and _c2 and debug and debug.info then
-            local n1 = debug.info(_c1, 'n')
-            local n2 = debug.info(_c2, 'n')
-            if tostring(n1) ~= tostring(n2) then
-                _hookDetected = true
-            end
-        end
-    end)
-    if _hookDetected then
-        game:GetService('Players').LocalPlayer:Kick('[AEROV4] integrity check failed')
-        error('[AEROV4] loadstring hook detected', 2)
-    end
-end
-
 local queue_on_teleport = queue_on_teleport or function() end
 local isfile = isfile or function(file)
-    local suc, res = pcall(function() return readfile(file) end)
-    return suc and res ~= nil and res ~= ''
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
 end
-
-local cloneref = cloneref or function(obj) return obj end
+local cloneref = cloneref or function(obj)
+	return obj
+end
 local playersService = cloneref(game:GetService('Players'))
-local httpService = cloneref(game:GetService('HttpService'))
 
 local function downloadFile(path, func)
-    if not isfile(path) then
-        local res
-        local success = false
-        for attempt = 1, 3 do
-            local suc, result = pcall(function()
-                return game:HttpGet('https://raw.githubusercontent.com/genvx/Nyxv7/' .. readfile('newvape/profiles/commit.txt') .. '/' .. select(1, path:gsub('newvape/', '')), true)
-            end)
-            if suc and result ~= '404: Not Found' then
-                res = result
-                success = true
-                break
-            end
-            task.wait(1)
-        end
-        if not success then
-            error('Failed to download ' .. path .. ' after 3 attempts')
-        end
-        if path:find('.lua') then
-            res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n' .. res
-        end
-        writefile(path, res)
-    end
-    return (func or readfile)(path)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/genvx/Nyxv7/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
 end
-
-local function migrateProfiles()
-    if isfile('newvape/profiles/migrated_placeid.txt') then return end
-    local oldId = tostring(game.GameId)
-    local newId = tostring(game.PlaceId)
-    if oldId == newId then
-        pcall(writefile, 'newvape/profiles/migrated_placeid.txt', 'done')
-        return
-    end
-    local suffix = oldId .. '.txt'
-    for _, path in ipairs(listfiles('newvape/profiles')) do
-        local name = path:gsub('\\', '/')
-        if name:sub(-#suffix) == suffix then
-            local newPath = name:sub(1, -#suffix - 1) .. newId .. '.txt'
-            if not isfile(newPath) then
-                pcall(function() writefile(newPath, readfile(path)) end)
-            end
-        end
-    end
-    if isfolder('newvape/profiles/premade') then
-        for _, path in ipairs(listfiles('newvape/profiles/premade')) do
-            local name = path:gsub('\\', '/')
-            if name:sub(-#suffix) == suffix then
-                local newPath = name:sub(1, -#suffix - 1) .. newId .. '.txt'
-                if not isfile(newPath) then
-                    pcall(function() writefile(newPath, readfile(path)) end)
-                end
-            end
-        end
-    end
-    pcall(writefile, 'newvape/profiles/migrated_placeid.txt', 'done')
-end
-pcall(migrateProfiles)
 
 local function finishLoading()
-    vape.Init = nil
-    if not vape.Load then
-        warn('[AEROV4] vape.Load is nil skipping load')
-        return
-    end
-    vape:Load()
-    vape:Clean(task.spawn(function()
-        repeat
-            pcall(vape.Save, vape)
-            task.wait(10)
-        until vape.Loaded == nil
-    end))
+	vape.Init = nil
+	vape:Load()
+	task.spawn(function()
+		repeat
+			vape:Save()
+			task.wait(10)
+		until not vape.Loaded
+	end)
 
-    local teleportedServers
-    vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-        if (not teleportedServers) and (not shared.VapeIndependent) then
-            teleportedServers = true
-            local teleportScript = [[
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/genvx/Nyxv7/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
-            ]]
-            if shared.VapeDeveloper then
-                teleportScript = 'shared.VapeDeveloper = true\n' .. teleportScript
-            end
-            if shared.VapeCustomProfile then
-                teleportScript = 'shared.VapeCustomProfile = "' .. shared.VapeCustomProfile .. '"\n' .. teleportScript
-            end
-            if shared.ValidatedUsername then
-                teleportScript = 'shared.ValidatedUsername = "' .. shared.ValidatedUsername .. '"\n' .. teleportScript
-            end
-            pcall(function() vape:Save() end)
-            queue_on_teleport(teleportScript)
-        end
-    end))
+	local teleportedServers
+	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
+		if (not teleportedServers) and (not shared.VapeIndependent) then
+			teleportedServers = true
+			local teleportScript = [[
+				shared.vapereload = true
+				if shared.VapeDeveloper then
+					loadstring(readfile('newvape/loader.lua'), 'loader')()
+				else
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/genvx/Nyxv7/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+				end
+			]]
+			if shared.VapeDeveloper then
+				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+			end
+			if shared.VapeCustomProfile then
+				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			end
+			vape:Save()
+			queue_on_teleport(teleportScript)
+		end
+	end))
 
-    if not shared.vapereload then
-        if not vape.Categories then return end
-        if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-            local name = shared.ValidatedUsername and ('wsg, ' .. shared.ValidatedUsername .. ' :D ') or 'welcome '
-            vape:CreateNotification('[AEROV4] Finished Loading', name .. (vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press ' .. table.concat(vape.Keybind, ' + '):upper() .. ' to open GUI'), 5)
-        end
-    end
+	if not shared.vapereload then
+		if not vape.Categories then return end
+		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
+			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+		end
+	end
 end
 
 if not isfile('newvape/profiles/gui.txt') then
-    writefile('newvape/profiles/gui.txt', 'new')
+	writefile('newvape/profiles/gui.txt', 'new')
 end
-
 local gui = readfile('newvape/profiles/gui.txt')
-if not isfolder('newvape/assets/' .. gui) then
-    makefolder('newvape/assets/' .. gui)
-end
 
-local guiSource = downloadFile('newvape/guis/' .. gui .. '.lua')
-local guiFunc, guiErr = _realLoadstring(guiSource, 'gui')
-if not guiFunc then
-    error('[AEROV4] syntax error in ' .. gui .. '.lua')
+if not isfolder('newvape/assets/'..gui) then
+	makefolder('newvape/assets/'..gui)
 end
-
-vape = guiFunc()
-if not vape then
-    error('[AEROV4] GUI returned nil')
-end
-
+vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
-task.wait(0.1)
-
--- Simplified Tier (no whitelist/website)
-getgenv().getAeroTier = function() return 99 end
-getgenv()._aeroTierReady = true
-getgenv().getAccountTier = function() return 99 end
-
-if getgenv().Closet then
-    local LogService = cloneref(game:GetService('LogService'))
-    local originals = {}
-    local function hook(funcName)
-        if typeof(getgenv()[funcName]) == 'function' then
-            local original = hookfunction(getgenv()[funcName], function() end)
-            originals[funcName] = original
-        end
-    end
-    hook('print')
-    hook('warn')
-    hook('error')
-    hook('info')
-    pcall(function() LogService:ClearOutput() end)
-    local conn = LogService.MessageOut:Connect(function()
-        LogService:ClearOutput()
-    end)
-    getgenv()._vape_log_connection = conn
-    getgenv()._vape_originals = originals
-end
 
 if not shared.VapeIndependent then
-    _realLoadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-
-    local gameFileId = (game.GameId == 2619619496) and (game.PlaceId == 6872265039 and 6872265039 or 6872274481) or game.PlaceId
-
-    if isfile('newvape/games/' .. gameFileId .. '.lua') then
-        _realLoadstring(downloadFile('newvape/games/' .. gameFileId .. '.lua'), tostring(gameFileId))(...)
-    else
-        if not shared.VapeDeveloper then
-            local suc, res = pcall(function()
-                return game:HttpGet('https://raw.githubusercontent.com/genvx/Nyxv7/' .. readfile('newvape/profiles/commit.txt') .. '/games/' .. gameFileId .. '.lua', true)
-            end)
-            if suc and res ~= '404: Not Found' then
-                _realLoadstring(downloadFile('newvape/games/' .. gameFileId .. '.lua'), tostring(gameFileId))(...)
-            end
-        end
-    end
-
-    finishLoading()
+	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
+	if isfile('newvape/games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+	else
+		if not shared.VapeDeveloper then
+			local suc, res = pcall(function()
+				return game:HttpGet('https://raw.githubusercontent.com/genvx/Nyxv7/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+			end)
+			if suc and res ~= '404: Not Found' then
+				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+			end
+		end
+	end
+	finishLoading()
 else
-    vape.Init = finishLoading
-    return vape
+	vape.Init = finishLoading
+	return vape
 end
